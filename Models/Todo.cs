6902c857e92ab2ed49f039035d40939f1425f79c
davidjeagle <47541872+davidjeagle@users.noreply.github.com>
2024-07-01 22:29:48 +0000

    Added property

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -12,5 +12,7 @@ namespace DotNetCoreSqlDb.Models
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
+
+        public int SecondID { get; set; }
     }
 }
